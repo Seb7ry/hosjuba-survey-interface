0793ccbbb5b0a23a5658d66a5f5b8b403ee1a921
Sebastian Murillo <sebasty06@outlook.com>
2025-06-16 11:58:15 -0500

    version 8

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   const accessToken = sessionStorage.getItem("access_token");
 
   useEffect(() => {
-    if (accessToken && window.location.pathname === "#/") {
+    if (accessToken && window.location.hash === "#/") {
       navigate("/dashboard");
     }
   }, [accessToken, navigate]);
