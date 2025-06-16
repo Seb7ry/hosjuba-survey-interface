db9ab56180b2a1cd642aa69a3603b4c7193b23ab
Sebastian Murillo <sebasty06@outlook.com>
2025-06-16 11:38:54 -0500

    version 6

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import './index.css'; 
 
@@ -8,8 +8,8 @@ const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter> 
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
